842c2ab6e1772968628204335a9d5a2aa8995d9e
BRYAN FARRO (DOCU-IRIS-ISD-OOCLL/MNL) <FARROBR@OOCL.COM>
2019-10-14 16:03:50 +0800

    finished more

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -1,29 +1,36 @@
 package com.tws.refactoring.extract_method;
 
-import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Vector;
 
 public class OwingPrinter {
     void printOwing(String name, List<Order> orders) {
         Iterator<Order> elements = orders.iterator();
+
+        printHeader();
+        printDetails(name, elements);
+
+    }
+
+    void printHeader() {
+        System.out.println("*****************************");
+        System.out.println("****** Customer totals ******");
+        System.out.println("*****************************");
+    }
+
+    public double printOwings(Iterator<Order> elements) {
         double outstanding = 0.0;
 
-        // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
-
-        // print owings
         while (elements.hasNext()) {
             Order each = (Order) elements.next();
             outstanding += each.getAmount();
         }
+        return outstanding;
+    }
 
-        // print details
+    void printDetails(String name, Iterator<Order> elements) {
         System.out.println("name: " + name);
-        System.out.println("amount: " + outstanding);
+        System.out.println("amount: " + printOwings(elements));
     }
 }
 
