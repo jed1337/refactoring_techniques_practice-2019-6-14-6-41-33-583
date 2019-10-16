9232868ff69576f73fc5fee3f771063238d9057d
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-16 16:21:59 +0800

    Refactor magic numbers in PriceCalculator to constants

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -1,10 +1,14 @@
 package com.tws.refactoring.extract_variable;
 
 public class PriceCalculator {
+    private static final double DISCOUNT_RATE = 0.05;
+    private static final double SHIPPING_COST_PER_ITEM = 0.1;
+    private static final double MAXIMUM_SHIPPING_COST = 100.0;
+
     double getPrice(int quantity, int itemPrice) {
         double basePrice = quantity * itemPrice;
-        double discount = Math.max(0, quantity - 500) * itemPrice * 0.05;
-        double shippingCost = Math.min(quantity * itemPrice * 0.1, 100.0);
+        double discount = Math.max(0, quantity - 500) * itemPrice * DISCOUNT_RATE;
+        double shippingCost = Math.min(quantity * itemPrice * SHIPPING_COST_PER_ITEM, MAXIMUM_SHIPPING_COST);
 
         return basePrice - discount + shippingCost;
     }
